4481ad00f7c0f94dd47c6b98c6cc4c0f46690660
MohammedRasif <mohammed660012@gmail.com>
2025-09-28 20:15:33 +0600

    add some new

--- src/component/Page.jsx/Marque.jsx
+++ src/component/Page.jsx/Marque.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
+import image from "../../image/Image (26).png";
 
 function Marque() {
   return (
     <div>
-        <p className="text-gray-300 pl-10 py-5 pb-16 text-center">
-            Works with the devices and labs you already use
-          </p>
+      <p className="text-gray-300 pl-10 py-5 pb-16 text-center">
+        Works with the devices and labs you already use
+      </p>
 
       <Marquee>
         <div className="flex space-x-40">
@@ -17,6 +18,26 @@ function Marque() {
           <h1 className="text-gray-400 text-5xl font-semibold">WHOOP</h1>
         </div>
       </Marquee>
+
+      <div className="relative">
+        <div>
+          <img src={image} className="w-full h-96 object-cover mt-16" alt="" />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center">
+          <h1 className="text-white text-5xl font-semibold max-w-2xl">
+            Health, Orchestrated. Longevity, Unlocked.
+          </h1>
+          <p className="text-gray-300 max-w-4xl py-5">
+            Cadensa is more than an app—it's the world's first Longevity
+            Operating System. By integrating your health data and guiding you
+            with adaptive insights, we help you unlock your full potential for a
+            longer, better life.
+          </p>
+          <p className="text-white">
+            "Add years to your life by adding life to your years."
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
